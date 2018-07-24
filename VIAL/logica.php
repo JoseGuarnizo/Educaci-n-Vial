d642d03510a4b073902315719152efded7717038
JoseGuarnizo <joseguarnizo1996@gmail.com>
2018-07-24 17:44:11 -0500

    Subo las actividades de GP libro q y 2 de Educacion Vial

---
+++ VIAL/logica.php
@@ -0,0 +1,40 @@
+<?php
+
+@$datos = array(
+	@"nombre" => $_POST['nombre'],
+);
+
+@$archivo = fopen($_POST['data'].".dat", "a");
+
+fwrite($archivo, json_encode($datos));
+fclose($archivo);
+
+?>
+
+<html>
+<body>
+	<head>
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/stylelogic.css">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>		
+	</head>
+
+	<style type="text/css">
+
+	</style>
+	
+	<div id="contenedor">
+		<center>
+			<h3>LIBROS DE TRABAJO</h3>
+			<a title="luces" href="http://localhost:8081/VIAL/internas/menu_actividad_segundo.html"><img src="imagenes/luces.png" alt="luces"/></a>
+			</br>
+			</br>
+			</br>
+			<a title="seguro" href="http://localhost:8081/VIAL/internas/menuactividad.html"><img src="imagenes/seguro.png" alt="seguro"/></a>
+		</center>
+		</br>
+		</br>
+	</div>
+
+</body>
+</html>
